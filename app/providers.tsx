00785ca4c0560f7b96b2d72173db2b4b7b65755e
AyushScripts <ayushtheking117@gmail.com>
2024-09-30 19:27:06 +0530

    Removed Convex integration

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,22 +1,15 @@
-'use client'
+"use client";
 
 import { ClerkProvider, useAuth } from "@clerk/nextjs";
-import { ConvexReactClient } from "convex/react";
-import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { PropsWithChildren, ReactNode } from "react";
 import { ThemeProvider } from "./components/theme-provider";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
-
-export function Providers({children} : PropsWithChildren) {
-    return (
-    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
-      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-      
-        {children}
-      
-      </ConvexProviderWithClerk>
+export function Providers({ children }: PropsWithChildren) {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      {children}
     </ClerkProvider>
-    )
+  );
 }
-
--- convex/auth.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    providers: [
-      {
-        domain: "https://composed-seal-93.clerk.accounts.dev",
-        applicationID: "convex",
-      },
-    ]
-  };
